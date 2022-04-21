7f137c54af11fe5cd1cf899bed62bd337cb012f4
Douglas Faria <hfsdouglas@gmail.com>
2022-04-21 19:45:34 -0300

    Refatoração do código utilizando o padrão MVC

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,1 @@
-var fields = document.querySelectorAll('#form-user-create [name]')
-var user = {}
-
-document
-  .getElementById('form-user-create')
-  .addEventListener('submit', event => {
-    event.preventDefault()
-    fields.forEach(field => {
-      if (field.name == 'gender') {
-        if (field.checked) {
-          user[field.name] = field.value
-        }
-      } else {
-        user[field.name] = field.value
-      }
-    })
-    console.log('OK')
-  })
+let userController = new UserController('form-user-create', 'table-users')
